6e1f0607cee8cb194091405fcc400a815dc3a238
WetPirat1 <EvgenSemydosky@ukr.net>
2024-12-14 15:00:31 +0200

    TelegramBotBanner done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Footer from "./components/common/Footer";
 import Header from "./components/common/Header";
 import HowWorks from "./components/sections/HowWorks";
 import TrustSection from "./components/sections/TrustSection";
+import TelegramBotBanner from "./components/ui/TelegramBotBanner";
 import "./tailwind.css";
 
 function App() {
   return (
     <>
+      <TelegramBotBanner />
       <Header />
       <TrustSection />
       <HowWorks />
