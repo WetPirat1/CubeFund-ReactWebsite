d7722284724ab6901b61959207a0d7d9c6823cc8
WetPirat1 <EvgenSemydosky@ukr.net>
2024-12-16 19:14:49 +0200

    footer adap done

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -4,12 +4,13 @@ import Logo_footer from "../../assets/Logo_footer";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="max-w-6xl flex justify-between items-center mx-auto">
-        <div className="flex gap-2">
+    <footer className="bg-gray-100 py-6">
+      <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row md:justify-between items-center gap-4">
+        <div className="flex items-center gap-2 text-center md:text-left">
           <Logo_footer />
-          <h3 className="">Cube Fund Invest</h3>
+          <h3 className="text-lg font-semibold">Cube Fund Invest</h3>
         </div>
+
         <SocialLinks />
       </div>
     </footer>
