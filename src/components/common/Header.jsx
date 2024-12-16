9bccfead6cb22b26170a9a3ea5e527c1b11cd559
Eugene <126335909+WetPirat1@users.noreply.github.com>
2024-12-16 14:36:50 +0200

    Revert "FloatingSquares layout in all section fix + Hero from Header"

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,17 +1,7 @@
-import React from "react";
-import SocialLinks from "../ui/SocialLinks";
-import TelegramLink from "../ui/TelegramLink";
-import Logo_nav from "../../assets/Logo_nav";
+import React from 'react'
 
 export default function Header() {
   return (
-    <header className=" max-w-5xl mx-auto mt-8">
-      <nav className="flex justify-between mb-8">
-        <a href="#">
-          <Logo_nav />
-        </a>
-        <SocialLinks />
-      </nav>
-    </header>
-  );
+    <div>Header</div>
+  )
 }
