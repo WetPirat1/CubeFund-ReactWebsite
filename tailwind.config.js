a1d01dfea67c79c2a49878df034f3efa4f51f9bf
Daniel Hash <hashdan542@gmail.com>
2025-01-03 22:56:55 +0100

    fixed module tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Путь к твоим файлам
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       animation: {
-        float: "float 6s ease-in-out infinite", // Плавное движение кубиков
+        float: "float 6s ease-in-out infinite",
       },
       keyframes: {
         float: {
           "0%, 100%": { transform: "translateY(0)" },
-          "50%": { transform: "translateY(-20px)" }, // Движение вверх
+          "50%": { transform: "translateY(-20px)" },
         },
       },
     },
